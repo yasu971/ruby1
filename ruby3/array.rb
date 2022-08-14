94c1afadf3d1334a86262a8e0a70af41bd2b91f6
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2022-08-14 13:29:22 +0900

    Update04_array.rb

--- ruby3/array.rb
+++ ruby3/array.rb
@@ -1,6 +1,6 @@
 pencil_case = ["ペン", "消しゴム", "定規"]
-pencil_case << "えんぴつ"
-pencil_case[1] = "修正ペン"
+puts pencil_case.length
 
-puts pencil_case
-puts pencil_case[1]
+pencil_case << "えんぴつ"
+pencil_case << "付箋"
+puts pencil_case.length
