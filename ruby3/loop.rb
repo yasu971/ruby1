0d875efb1bb730140d098ecb6384adf097fabd88
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2022-08-28 14:01:59 +0900

    Update01_loop.rb

--- ruby3/loop.rb
+++ ruby3/loop.rb
@@ -1,3 +1,10 @@
 10.times do |i|
     puts i + 1
   end
+
+  colors = ["あか", "あお", "きいろ"]
+element_count = colors.length  # 要素数を変数に代入
+
+element_count.times do |i|
+   puts "色: #{colors[i]}"  # 添字0から要素を出力
+end
